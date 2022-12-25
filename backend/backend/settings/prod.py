ebc3dca6370646328c4a53c4a71a67e93321ca6d
David Deisadze <ddeisadze@outlook.com>
2022-12-25 14:53:11 -0500

    Fix settings variables

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,6 +1,6 @@
-from backend.settings.common import *
 import os
 import dj_database_url
+from backend.settings.common import *
 
 DEBUG = False
 
@@ -16,11 +16,11 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(        
-        # Feel free to alter this value to suit your needs.        
-        default='postgresql://postgres:postgres@localhost:5432/mysite',        
-        conn_max_age=600    )}
+# DATABASES = {
+#     'default': dj_database_url.config(        
+#         # Feel free to alter this value to suit your needs.        
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',        
+#         conn_max_age=600    )}
 
 MIDDLEWARE.extend([
     'django.middleware.security.SecurityMiddleware',
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+if os.environ.get("DEPLOYMENT_ENVIRONMENT") == "prod":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
 
 application = get_wsgi_application()
