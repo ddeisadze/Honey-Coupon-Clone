b4d1f26567fc115fc9004f4319a9dff25644a3e2
David Deisadze <ddeisadze@outlook.com>
2022-12-25 13:23:58 -0500

    Environment changes

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
+    
+    if os.environ.get("DEPLOYMENT_ENVIRONMENT") == "prod":
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
