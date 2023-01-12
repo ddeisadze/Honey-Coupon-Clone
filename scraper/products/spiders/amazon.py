6d81ebfbe03a52dba5d2431f62b764046fff58c3
David Deisadze <tbilisidavid@hotmail.com>
2023-01-11 22:09:39 -0500

    Clean out crawlers. Add docker compose

--- scraper/products/spiders/amazon.py
+++ scraper/products/spiders/amazon.py
@@ -1,96 +1,10 @@
 import scrapy
-from scrapy.loader import ItemLoader
 import re
 import json
 from urllib.parse import urlencode
 from urllib.parse import urljoin
 
 from products.items import AmazonProductItem
-
-# class Product(scrapy.Item):
-#     name = scrapy.Field()
-#     price = scrapy.Field()
-#     stock = scrapy.Field()
-#     tags = scrapy.Field()
-#     last_updated = scrapy.Field(serializer=str)
-
-
-class SlickDeals(scrapy.Spider):
-    name = "slickdeals-computerdeals"
-
-    def start_requests(self):
-        url = "https://slickdeals.net/computer-deals"
-        yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response, **kwargs):
-        # l = ItemLoader(item=Product(), response=response)
-        items_on_page = response.css("ul.dealTiles li")
-        # css(".bp-c-button--link::text").get()
-
-
-def parse_tables(response):
-    # Define a list to store the parsed tables
-    tables = []
-
-    # Extract all table elements from the HTML
-    elements = response.xpath('//table')
-
-    # Iterate over the table elements and parse them
-    for element in elements:
-        try:
-            table = parse_table(element)
-            if table:
-                table_id = "-".join(element.xpath(
-                    './ancestor-or-self::div/@id').extract()[-3:])
-                tables.append({"id": table_id, "data": table})
-        except Exception as e:
-            pass
-
-    # Return the list of parsed tables
-    return tables
-
-
-def parse_table(table):
-    # Define a dictionary to store the parsed values
-    data = {}
-
-    # Extract the rows from the table
-    rows = table.xpath('./tr')
-    if len(rows) == 0:
-        return None
-
-    def clean_list_of_null_values(list_of_values):
-        return [i for i in list_of_values if i.strip()]
-
-    # Extract the headers from the first row
-    headers = clean_list_of_null_values(rows[0].xpath(
-        './th/descendant-or-self::text()').extract())
-    # Check if the headers are in th elements or td elements
-    if headers:
-        # If the headers are in th elements, use them as keys
-        for i, header in enumerate(headers):
-            data[str(header)] = []
-        # Iterate over the remaining rows and extract the values
-        for row in rows[1:]:
-            values = clean_list_of_null_values(
-                row.xpath('./td/text() | ./td/descendant-or-self::span/text()').extract())
-            for i, value in enumerate(values):
-                data[str(headers[i])].append(value)
-    else:
-        # If the headers are not present, use the indices as keys
-        for row in rows:
-            values = clean_list_of_null_values(
-                row.xpath('./td/descendant-or-self::span/text()').extract())
-
-            for i, value in enumerate(values):
-                if i not in data:
-                    data[str(i)] = []
-                data[str(i)].append(value)
-
-    # Return the dictionary as a JSON string
-    if (len(data) > 0):
-        return data
-    return None
 
 
 class AmazonSearchToProductPage(scrapy.Spider):
@@ -101,10 +15,14 @@ class AmazonSearchToProductPage(scrapy.Spider):
     base_url = "www.amazon.com"
     current_search_term = None
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, search_term=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.search_term = search_term
 
     def start_requests(self):
+        if self.search_term:
+            self.search_terms = [self.search_term]
+
         for term in self.search_terms:
             self.current_search_term = term
             url = 'https://www.amazon.com/s?' + urlencode({'k': term})
@@ -125,33 +43,37 @@ class AmazonSearchToProductPage(scrapy.Spider):
             url = urljoin("https://www.amazon.com", next_page)
             yield scrapy.Request(url=url, callback=self.parse_keyword_response)
 
-# class AmazonBestSellerList(scrapy.Spider):
-#     name = 'amazon_best_seller_list'
-#     base_url = "www.amazon.com"
-#     best_seller_category_pages = []
-
-#     def start_requests(self):
-#         for term in self.best_seller_category_pages:
-#             url = 'https://www.amazon.com/s?' + urlencode({'k': term})
-#             yield scrapy.Request(url=url, callback=self.parse_keyword_response)
-
 
 class AmazonProductPage(scrapy.Spider):
     name = 'amazon_page'
     base_url = "www.amazon.com"
 
-    def __init__(self, asin, *args, **kwargs):
+    def __init__(self, asin=None, url=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.asin = asin
+        self.url = url
 
     def start_requests(self):
-        url = f"https://www.amazon.com/dp/{self.asin}"
-        yield scrapy.Request(url=url, callback=parse_product_page, meta={'asin': self.asin})
+        if self.url:
+            url = self.url
+        elif self.asin:
+            url = f"https://www.amazon.com/dp/{self.asin}"
+        else:
+            raise ValueError("Either url or asin must be provided")
+
+        yield scrapy.Request(url=url, callback=parse_product_page, meta={'asin': self.asin, 'url': self.url})
 
 
 def parse_product_page(response):
-    asin = response.meta['asin']
-    product_url = f"https://www.amazon.com/dp/{asin}"
+    asin = None
+    if 'asin' in response.meta:
+        asin = response.meta['asin']
+        product_url = f"https://www.amazon.com/dp/{asin}"
+    elif 'url' in response.meta:
+        product_url = response.meta['url']
+    else:
+        raise ValueError("Either url or asin must be provided")
+
     title = response.xpath(
         '//*[@id="productTitle"]/text()').extract_first()
     try:
