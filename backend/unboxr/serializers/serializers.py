51386383e23b90344e2f3fb01f89d03b8426c8fc
David Deisadze <tbilisidavid@hotmail.com>
2023-02-11 17:48:37 -0500

    update docker compose and first stages of model redesign

--- backend/unboxr/serializers/serializers.py
+++ backend/unboxr/serializers/serializers.py
@@ -3,29 +3,32 @@ from unboxr.models import Product, Promotion, Influencer, InfluencerSocialMedia,
 
 
 social_choices = (
-        ('Tik', 'Tik Tok'),
-        ('Insta', 'Instagram'),
-        ('Fb', 'Facebook'),
-        ('Yt', 'Youtube')
-    )
-
+    ('Tik', 'Tik Tok'),
+    ('Insta', 'Instagram'),
+    ('Fb', 'Facebook'),
+    ('Yt', 'Youtube')
+)
 
 
 class InfluencerSocialMediaSerializer(serializers.ModelSerializer):
-    platform = serializers.ChoiceField(choices=social_choices, source='social_media_type')
+    platform = serializers.ChoiceField(
+        choices=social_choices, source='social_media_type')
     username = serializers.CharField(source='social_media_username')
 
     class Meta:
         model = InfluencerSocialMedia
         fields = ['platform', 'username']
 
+
 class InfluencerSerializer(serializers.ModelSerializer):
-    social_medias = InfluencerSocialMediaSerializer(many=True, read_only=True, source='social_media')
+    social_medias = InfluencerSocialMediaSerializer(
+        many=True, read_only=True, source='social_media')
 
     class Meta:
         model = Influencer
         fields = ['name', 'description', 'social_medias']
         depth = 2
+
 
 class CouponSerializer(serializers.ModelSerializer):
     class Meta:
@@ -33,22 +36,24 @@ class CouponSerializer(serializers.ModelSerializer):
         fields = ['coupon_code']
         depth = 1
 
+
 class ProductPriceSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = ProductPrice
-        fields = ['price', 'date_published']
+        fields = '__all__'
         depth = 1
-
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    product_price_history = ProductPriceSerializer(many=True, read_only=True, source='ProductPrice')
+    prices = ProductPriceSerializer(many=True, read_only=True, source='prices')
 
     class Meta:
         model = Product
-        fields = ['product_name', 'company_name', 'company_website', 'product_price', 'merchant_product_page', 'product_description', 'product_images', 'product_ids', 'product_categories', 'product_price_history']
+        fields = ['product_name', 'company_name', 'company_website', 'product_price', 'merchant_product_page',
+                  'product_description', 'product_images', 'product_ids', 'product_categories', 'prices']
         depth = 1
+
 
 class PromotionSerializer(serializers.ModelSerializer):
     influencer = InfluencerSerializer(read_only=True)
@@ -61,5 +66,6 @@ class PromotionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Promotion
-        fields = ['influencer', 'product', 'videos', 'images', 'social_media_type', 'coupon_description', 'coupon_code_in_the_link', 'post_link', 'post_promotion_date', 'promotion_expiration_date', 'advertisement_link', 'date_modified', 'coupons']
+        fields = ['influencer', 'product', 'videos', 'images', 'social_media_type', 'coupon_description', 'coupon_code_in_the_link',
+                  'post_link', 'post_promotion_date', 'promotion_expiration_date', 'advertisement_link', 'date_modified', 'coupons']
         depth = 1
--- backend/unboxr/testData/build_test_data.py
+++ backend/unboxr/testData/build_test_data.py
@@ -1,12 +1,9 @@
-from django.test import TestCase
+# from django.contrib.auth.models import User
 from unboxr.models import Product_Category, ProductImages, Product, ProductPrice, InfluencerSocialMedia, ProductSkuId, ProductIdType, Influencer
-import pytest
 
 
-# Create your tests here.
-@pytest.fixture
-@pytest.mark.django_db(transaction=True)
-def fixture() -> None:
+def populate_test_data():
+    # Create test product categories
     product_category_1 = Product_Category.objects.create(
         name='Fashion',
         description='Clothing, shoes and accessories'
@@ -43,10 +40,9 @@ def fixture() -> None:
 
     product_sku_id = ProductSkuId.objects.create(
         product=product,
-        product_id_value='B0BCWNQPQ7'
+        product_id_value='B0BCWNQPQ7',
+        product_id_type=ProductIdType.objects.filter(name="ASIN")
     )
-    
-    product_sku_id.product_id_type.set(ProductIdType.objects.filter(name="ASIN"))
 
     influencer = Influencer.objects.create(
         name="Dan Driggs",
@@ -66,6 +62,3 @@ def fixture() -> None:
     ]
     for product_category in product_categories:
         Product_Category.objects.create(**product_category)
-
-def test(live_server, fixture):
-    print("hello")
