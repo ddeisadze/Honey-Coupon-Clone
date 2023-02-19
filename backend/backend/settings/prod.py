d6def0bcc08d4d99720e4fbf2114c1f955af2787
David Deisadze <tbilisidavid@hotmail.com>
2023-02-18 19:38:55 -0500

    allowed hosts

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -9,7 +9,7 @@ DEBUG = os.environ.get('DEBUG', default=False)
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
