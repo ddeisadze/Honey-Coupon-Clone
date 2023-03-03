8f23d625799de987e8176bcac21f0795b4d6dad6
David Deisadze <tbilisidavid@hotmail.com>
2023-03-02 19:49:34 -0500

    Cleanup

--- extension/src/background/backgroundIndex.tsx
+++ extension/src/background/backgroundIndex.tsx
@@ -1,35 +1,3 @@
-
-chrome.runtime.onStartup.addListener(function () {
-
-    console.log("hello")
-})
-
-const loginTabId = Math.random()
-
-// chrome.webRequest.onBeforeRequest.addListener((op) => {
-
-//     op.requestBody.
-// }, {
-//     urls: [`${process.env.BACKEND_HOSTNAME}/*`]
-// },
-//     ["Authorization", 'Token ' + await getAuthToken()]
-// )
-
-
-// chrome.webRequest.onBeforeSendHeaders.addListener(async (a) => {
-//     const tokenValue = await chrome.cookies.get({
-//         name: "unboxr_auth",
-//         url: process.env.COOKIE_URL
-//     });
-
-//     console.log(tokenValue, "token value")
-
-//     a.requestHeaders["Authorization"] = 'Token ' + tokenValue
-// }, {
-//     urls: [`${process.env.BACKEND_HOSTNAME}/*`]
-// });
-
-
 chrome.runtime.onMessage.addListener(async ({ type, name }, { tab }) => {
     if (type == "login") {
         var a = chrome.windows.create(
@@ -88,28 +56,3 @@ chrome.runtime.onMessage.addListener(async ({ type, name }, { tab }) => {
         console.log("Unrecognized")
     }
 });
-
-
-// chrome.runtime.onMessage.addListener(
-//     function (request, sender) {
-//         const tabId = sender.tab.id;
-
-//         if (request.action == "login") {
-
-//         } else {
-//             console.log("Unrecognized")
-//         }
-//         console.log(sender.tab ?
-//             "from a content script:" + sender.tab.url :
-//             "from the extension");
-//         if (request.greeting === "hello")
-//             sendResponse({ farewell: "goodbye" });
-
-//         (async () => {
-//             const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-//             const response = await chrome.tabs.sendMessage(tab.id, { greeting: "hello" });
-//             // do something with response here, not outside the function
-//             console.log(response);
-//         })();
-//     }
-// );
